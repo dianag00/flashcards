385692bce509c05520a64908d3ff8f3f662e741f
dianag00 <dguerrero1232@gmail.com>
2026-06-14 23:50:31 -0700

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import { useState } from 'react';
+
+const App = () => {
+
+  //  define card data here:
+  const cards = () => {
+    
+  }
+
+  return (
+    <div>
+      <h1>Hello World in 11</h1>
+      <p>How good at you at recognizing "Hello World!" in different programming languages? Test your knowledge here!</p>
+      <p>Number of cards: </p>
+    </div>
+  )
+
+}
+
+export default App;
