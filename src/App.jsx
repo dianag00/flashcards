53a5377f979a3ad9de809e025c899f939256d303
Diana <60504464+dianag00@users.noreply.github.com>
2026-06-21 23:46:36 -0700

    completed all requirements for project

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,51 @@ const App = () => {
   const [isFlipped, setIsFlipped] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0); // sets currentIndex default value of 0
 
+  const [userGuess, setUserGuess] = useState('');
+  const [checkAnswer, setCheckAnswer] = useState("");
+
   // define functions here:
 
   const nextCard = () => { 
-    setCurrentIndex(Math.floor(Math.random() * cards.length)); // sets current index to random number generator value upon clicking next button
+    // checks if index is within bounds in order to apply appropriate index for next card button
+    if (currentIndex != cards.length - 1) {
+      setCurrentIndex(currentIndex + 1) // increases index by 1 when user clicks on next button 
+    }
+    else {
+      setCurrentIndex(currentIndex);
+    }
+  
     setIsFlipped(false); // sets the isFlipped setter back to false upon clicking next button
+
+    setCheckAnswer('');
+  }
+
+
+
+      // checks whether userGuess matches the current card's answer
+  const onSubmit = () => {
+      if (userGuess === cards[currentIndex].answer) {
+        setCheckAnswer("correct");
+      }
+      else {
+        setCheckAnswer("incorrect");
+      }
+
+    }
+  
+  const prevCard = () => {
+    // checks if index is within bounds in order to apply appropriate index for previous card button
+    if (currentIndex != 0)  {// decrements index by 1 when user clicks on previous button but first checks if index isn't less than 0
+      setCurrentIndex(currentIndex - 1);
+    }
+    else {
+      setCurrentIndex(currentIndex);
+    }
+    
+    setIsFlipped(false); // sets the isFlipped setter back to false upon clicking next button
+    setCheckAnswer('');
+
+
   }
 
   return (
@@ -29,7 +69,21 @@ const App = () => {
       setIsFlipped={setIsFlipped} // setIsFlipped props literally takes in itself as a value
       />
 
-      <button onClick={nextCard}>Next Card</button>
+
+    <div className="input-container">
+      <div className="submit-button">
+        <button onClick={onSubmit}>Check answer:</button>
+      </div>
+      
+      <div className="answer-field">
+        <input className = {checkAnswer} value={userGuess}  type="text" onChange = {(e) => {setUserGuess(e.target.value)}}></input>
+      </div>
+  </div>
+
+
+
+      <button className = {currentIndex === cards.length - 1 ? "disabled" : ""}  onClick={nextCard}>Next Card</button>
+      <button className = {currentIndex === 0 ? "disabled" : ""} onClick={prevCard} >Previous Card</button>
 
 
 
